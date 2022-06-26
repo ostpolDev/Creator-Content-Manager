01511d2c3fffb37f03ca35937a6bcca3ab1f8911
ostpolDev <alexander.hitzinger@gmail.com>
2022-06-26 13:04:59 +0200

    Added video index page

--- routes/videos.js
+++ routes/videos.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const validation = require('../modules/validation');
 
 router.get("/", validation.ensureAuthenticated, (req, res) => {
-    
+    res.render('videos/index', {
+        title: "Videos"
+    })
 })
 
 module.exports = router;
