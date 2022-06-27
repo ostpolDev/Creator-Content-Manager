78089e8474a0eb2dc6deb58e177d83ad88ad4980
ostpolDev <alexander.hitzinger@gmail.com>
2022-06-27 21:42:29 +0200

    Added upload form

--- routes/assets.js
+++ routes/assets.js
@@ -7,7 +7,14 @@ const logger = require('../modules/logger');
 const Asset = require('../models/asset');
 const Batch = require('../models/batch');
 
-let fileTypes = [".jpg", ".png", ".mp3", ".mp4", ".wmv", ".webp"];
+const fileTypes = [".jpg", ".png", ".mp3", ".mp4", ".wmv", ".webp", ".ogg", ".jpeg", ".pdn"];
+const assetTypes = ["music", "soundEffect", "video", "image"];
+
+router.use("*", (req, res, next) => {
+    res.locals.fileTypes = fileTypes;
+    res.locals.assetTypes = assetTypes;
+    next();
+})
 
 router.get('/', (req, res, next) => {
     Asset.countDocuments({}).exec((err, count) => {
@@ -29,8 +36,7 @@ router.get('/', (req, res, next) => {
 
 router.get('/upload', validation.ensureAuthenticated, (req, res, next) => {
     res.render('assets/upload', {
-        title: "Upload assets",
-        fileTypes
+        title: "Upload assets"
     })
 })
 
