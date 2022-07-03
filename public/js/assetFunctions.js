17ac0b72aea25dd0995a0b740720175f12a9c708
ostpolDev <alexander.hitzinger@gmail.com>
2022-07-03 17:54:18 +0200

    Fixed music player cover

--- public/js/assetFunctions.js
+++ public/js/assetFunctions.js
@@ -11,19 +11,20 @@ async function playAsset(id) {
     currentAsset = await getAssetInfo(id);
     if (currentAsset) {
         let title = currentAsset.meta.hasCustomName ? currentAsset.name : currentAsset.cleanName;
-        let splits = title.split("-");
+
+        let batchTitle = currentAsset.batch.name;
+
+        let splits = batchTitle.split("-");
         let artist = "";
         if (splits.length > 1) {
             artist = splits[1];
-
-            title = splits[0];
         }
         
         showMusicPlayer({
             url: "/assets/getFile/"+encodeURIComponent(currentAsset._id),
             artist,
             title,
-            cover: currentAsset.batch.cover.hasCover ? "/batches/getCover/"+encodeURIComponent(currentAsset.batch._id) : undefined,
+            cover: currentAsset.batch.cover.hasCover ? "/assets/batches/cover/"+encodeURIComponent(currentAsset.batch._id) : undefined,
             page: "/assets/v/"+encodeURIComponent(currentAsset._id)
         })
     }
