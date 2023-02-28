bdd36056384e2197314a862bc74cacc9f84ff4b1
ostpolDev <alexander.hitzinger@gmail.com>
2023-03-01 00:54:46 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/'+process.env.MONGO_PATH,
+    database: 'mongodb://127.0.0.1:27017/'+process.env.MONGO_PATH,
     secret: process.env.MONGO_SECRET
 }
