ccfb70ca508bd6d941289b8cbf48a2f2bce77a49
ostpolDev <alexander.hitzinger@gmail.com>
2023-03-19 13:45:57 +0100

    Added game backend

--- routes/games.js
+++ routes/games.js
@@ -1,9 +1,112 @@
 const router = require('express').Router();
+const { body, validationResult } = require('express-validator');
+const Game = require('../models/game');
+const logger = require('../modules/logger');
+
+const steamGameHelper = require('../modules/steamGameHelper');
+
+const { ensureAuthenticated } = require('../modules/validation');
+
+router.get('*', ensureAuthenticated, (req, res, next) => {
+    next();
+})
 
 router.get("/", (req, res) => {
-    res.render('games/index', {
-        title: "Games"
+    Game.find().sort({"name": 1}).exec((err, games) => {
+        if (err) {
+            logger.error(err);
+        }
+        res.render('games/index', {
+            title: "Games",
+            games
+        })
     })
 })
+
+router.get('/v/:id', (req, res) => {
+    Game.findById(req.params.id).populate("meta.addedBy", "username name safeName id").exec((err, game) => {
+        if (err) {
+            logger.error(err);
+        }
+        if (game) {
+            res.render('games/view', {
+                game,
+                title: game.name
+            })
+        } else {
+            req.flash('danger', "Game not found");
+            res.redirect('/games');
+        }
+    })
+})
+
+router.get('/v/:id/images', (req, res) => {
+    Game.findById(req.params.id).exec((err, game) => {
+        if (err) {
+            logger.error(err);
+        }
+        if (game) {
+            res.render('games/images', {
+                game,
+                title: game.name
+            })
+        } else {
+            req.flash('danger', "Game not found");
+            res.redirect('/games');
+        }
+    })
+})
+
+router.get('/v/:id/videos', (req, res) => {
+    Game.findById(req.params.id).exec((err, game) => {
+        if (err) {
+            logger.error(err);
+        }
+        if (game) {
+            res.render('games/videos', {
+                game,
+                title: game.name
+            })
+        } else {
+            req.flash('danger', "Game not found");
+            res.redirect('/games');
+        }
+    })
+})
+
+router.post('/add', [
+    body("id", "Id is invalid").isNumeric()
+], async (req, res) => {
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        errors.array().forEach(e => {
+            req.flash('danger', e.msg);
+        })
+        res.redirect('/games/add');
+        return;
+    }
+    let id = req.body.id;
+    let data = await steamGameHelper.getAndCreate(id, req);
+    if (data.success === true) {
+        req.flash('success', "Successfully added game");
+    } else {
+        logger.error(data);
+        req.flash('danger', "Something went wrong...");    
+    }
+    res.redirect("/games/add");
+})
+
+router.get('/add', (req, res) => {
+    res.render('games/add', {
+        title: "Add game"
+    })
+})
+
+router.post('/update', async (req, res) => {
+    let id = req.body.id;
+    let data = await steamGameHelper.updateGameInfo(id, req);
+    res.status(200).json(data);
+})
+
 
 module.exports = router;
