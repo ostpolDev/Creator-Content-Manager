390ddc0c2989a78e999bcecaec0cd58ca91be120
ostpolDev <alexander.hitzinger@gmail.com>
2023-03-19 17:12:44 +0100

    Moved delete button

--- routes/videos.js
+++ routes/videos.js
@@ -127,7 +127,7 @@ router.get('/update/:id', validation.ensureAuthenticated, rateLimiter.apiRequest
     res.redirect('/videos/v/'+id);
 })
 
-let settings = ["general", "assets", "games", "change channel"];
+let settings = ["general", "assets", "games", "change channel", "delete"];
 
 router.get('/settings/:id', validation.ensureAuthenticated, validation.ensureChannel, (req, res, next) => {
     res.redirect('/videos/settings/'+encodeURIComponent(req.params.id)+"/"+encodeURIComponent(settings[0]));
