f4f07503e734a6011f49e83433046e582e685322
ostpolDev <alexander.hitzinger@gmail.com>
2023-03-27 20:02:46 +0200

    Added link replacement for description

--- public/js/videoLegalModal.js
+++ public/js/videoLegalModal.js
@@ -47,7 +47,15 @@ async function viewGeneratedDescription() {
         return;
     }
 
-    descriptionContent.innerText = json.description;
+    descriptionContent.innerHTML = replace_content(json.description);
     viewGeneratedDescriptionButton.classList.remove("loading");
     toggleModalQuery("#descriptionModal");
+}
+
+function replace_content(content) {
+    var exp_match = /(\b(https?|):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    var element_content=content.replace(exp_match, "<a href='$1' target='_BLANK'>$1</a>");
+    var new_exp_match =/(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    var new_content=element_content.replace(new_exp_match, '$1<a target="_blank" href="http://$2">$2</a>');
+    return new_content;
 }
